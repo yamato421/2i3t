eda1d8f3736e7e559200da840ad02691a68dfbb9
yamato421 <106193990+yamato421@users.noreply.github.com>
2023-09-11 10:23:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function alo_mundo(){
+    alert ("projeto iniciado! alo!")
+}
+
+
+escrever = msg => alert(msg);
+soma = (a.b) => a + b;
+sub = (a.b) => a - b;
+mult = (a.b) => a * b;
+div = (a.b) => a / b;
+raiz = a => Math.sqrt(a);
+equacao2Grau = (a,b,c) => {
+    let delta = sub(mult(b,b),mult(4,mult(a,c)));
+}
+
+//atempt 1
+escrever(soma(10,8));
+
+escrever(soma(1000, mult(1000,div(22,1000)));
