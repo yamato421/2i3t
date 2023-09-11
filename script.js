f4c35206d8ad4d1b6ca914ead11fa100eb476c44
yamato421 <106193990+yamato421@users.noreply.github.com>
2023-09-11 10:48:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ function alo_mundo(){
 
 
 escrever = msg => alert(msg);
-soma = (a.b) => a + b;
-sub = (a.b) => a - b;
-mult = (a.b) => a * b;
-div = (a.b) => a / b;
+soma = (a,b) => a + b;
+sub = (a,b)  => a - b;
+mult = (a,b) => a * b;
+div = (a,b)  => a / b;
 raiz = a => Math.sqrt(a);
 equacao2Grau = (a,b,c) => {
     let delta = sub(mult(b,b),mult(4,mult(a,c)));
+    if (delta < 0) return " Não possui raiz Real";
+    if (delta ==0) return  "x1 = x2 " + div(-b,mult(2,a));
+    return "x1 =" + div(soma(-b,raiz(delta)),mult(2,a)); + "x2 ="
+    + div(sub(-b,raiz(delta)),mult(2,a));
 }
 
 //atempt 1
-escrever(soma(10,8));
+escrever(equacao2Grau( 0,0,0))
 
-escrever(soma(1000, mult(1000,div(22,1000)));
